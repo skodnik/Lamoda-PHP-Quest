3fbc8f7a4185bdd2f20c47a2ab85a5a2e3aaa8e1
vlsv <g.skodnik@gmail.com>
2019-08-01 18:04:31 +0300

    routes, .env, migrations, edit

---
+++ database/migrations/2019_07_25_161412_create_containers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContainersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('containers', function (Blueprint $table) {
+            if ($_ENV['INCREMENT_IDS']) {
+                $table->bigIncrements('id');
+            } else {
+                $table->unsignedBigInteger('id')->unique();
+            }
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('containers');
+    }
+}
